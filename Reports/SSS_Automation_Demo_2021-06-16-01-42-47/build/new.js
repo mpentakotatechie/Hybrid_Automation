63d651b5922bee0206ca6c29859e665473239902
murali krishna <kpentakota29@gmail.com>
2021-06-23 18:56:32 +0530

    deleted reports

--- Reports/SSS_Automation_Demo_2021-06-16-01-42-47/build/new.js
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">
-function toggle(id) {
-    document.getElementById(id).style.display = (document.getElementById(id).style.display == "block")?"none":"block";
-}
-</script>
